683e7a0e66938624e260637362f2a5ed36f17e00
Ellis Franklin <ellis.franklin@univ-lorraine.fr>
2024-07-21 16:15:18 +0200

    refactor: update render_qmd.R
    - modify messages to improve readability
    - add condition to create path if not already existing

--- R/render_qmd.R
+++ R/render_qmd.R
@@ -13,16 +13,14 @@
 #'
 #' @examples
 
-render_qmd <- function(input_file, output_path, file_ext, ...) {
-  # Extract just the input file name (without the file-extension)
-  file_name <- xfun::sans_ext(input_file)
+render_qmd <- function(input_file, output_file, output_path, file_ext, ...) {
   
   # render the input document and output file will be in the
   # current working directory.
   quarto::quarto_render(input = input_file, output_format = file_ext, ...)
   
   # name of the rendered output file
-  output_name <- paste0(file_name, ".", file_ext)
+  output_name <- paste0(output_file, ".", file_ext)
   
   # Ensure the output directory exists
   if (!fs::dir_exists(output_path)) {
@@ -32,6 +30,9 @@ render_qmd <- function(input_file, output_path, file_ext, ...) {
   # move the file to the output path
   fs::file_move(paste0(output_name), output_path)
   
-  msg <- paste0(paste0(output_name, collapse = " and "), " moved to ", output_path)
-  message(msg)
+  message(paste0(paste0(output_name, collapse = " and "), " moved to ", output_path))
+  
+  message(paste0(output_name, collapse = " and ", "\n"))
+  message(paste0("--> moved to -->"))
+  message(output_path)
 }
