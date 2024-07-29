bff7a823a0a07e5d9a315c34cbff801621ae92dc
Ellis Franklin <ellis.franklin@univ-lorraine.fr>
2024-07-29 12:09:01 +0200

    chore: add comment to messages

--- R/dl_regulation_data.R
+++ R/dl_regulation_data.R
@@ -33,12 +33,14 @@ dl_regulation_data <- function(
     dir.create(path, showWarnings = FALSE, recursive = TRUE)
     # `recursive = TRUE` creates intermediate directories as needed
     
+    # Message stating that the directory is created
     message("Directory '", path, "' created.")
   }
   
+  # Check if files exist locally and overwrite is not set to TRUE
   if (file.exists(file.path(path, filename_tf)) & !overwrite) {
     
-    ## Check if files exist locally and overwrite is not set to TRUE
+    # Message stating that the files already exist, and 'overwrite = TRUE' as to be set in order to replace them
     message("The TF and Cof filenames already exists. Use 'overwrite = TRUE' to replace them")
     
   } else {
