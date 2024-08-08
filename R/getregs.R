351f1b9b919775e89e6e1f4e563af2a5a02a3f7a
Ellis Franklin <ellis.franklin@univ-lorraine.fr>
2024-08-08 10:54:26 +0200

    feat: add messages for certain cases
      - stop function if the at least one of the two regulator files is not
        found in the specified paths
      - stop function if the 'Ensembl' column is missing from at least one
        of the two regulator files

--- R/getregs.R
+++ R/getregs.R
@@ -1,7 +1,7 @@
 #' Retrieve the regulatory status of deregulated genes
 #' 
 #' @description
-#' This function retrieves information on the regulatory status of genes using the files downloaded with the `dl_regulation_data()` function.
+#' This function retrieves information on the regulatory status of genes using the files downloaded with the `dl_regulation_data()` function. It firstly checks if the specifed files have beel downloaded, and whether they contain the è `Ensembl`
 #' 
 #' @param getids_data A dataframe of type *t* that typically corresponds to the output of the `getids()` function. This input holds at least one column named "gene_id" holding gene identifiers for the deregulated genes.  It should be a subset of the output of the `getids()` function with only the significantly deregulated transcripts as rows and transcript/gene identifiers as columns.
 #' @param regulator_file The previously downloaded `.txt` Transcription Factor file 
@@ -20,22 +20,47 @@ getregs <- function(
     coregulator_file 
     )
 {
-  # Read the files containing Transcription Factors and Co-Regulators data
+  # Check if the regulator and co-regulator files exist
+  if (!file.exists(regulator_file)) {
+    
+    stop("The file specified for TF annotations does not exist in this path: ", regulator_file)
+    
+  }
+  
+  if (!file.exists(coregulator_file)) {
+    
+    stop("The file specified for coTF annotations does not exist in this path: ", coregulator_file)
+    
+  }
+  
+  # Read the transcription factor and co-factor data files
   g_coregs_data <- read.table(coregulator_file, sep = "\t", header = TRUE)
   g_regs_data <- read.table(regulator_file, sep = "\t", header = TRUE)
   
-  # Create the vector of the Ensembl genes that are co-regulators
+  # Check if the required columns are present
+  if (!"Ensembl" %in% colnames(g_regs_data)) {
+    
+    stop("The TF file is missing the 'Ensembl' column.")
+    
+  }
+  
+  if (!"Ensembl" %in% colnames(g_coregs_data)) {
+    
+    stop("The coTF file is missing the 'Ensembl' column.")
+    
+  }
+  
+  
+  # Extract vectors of Ensembl gene identifiers for co-regulators and regulators
   coregs_items <- as.vector(g_coregs_data$Ensembl) 
+    regs_items <- as.vector(g_regs_data$Ensembl) 
   
-  # Create the vector of the Ensembl genes that are regulators
-  regs_items <- as.vector(g_regs_data$Ensembl) 
-  
-  # Combine both vectors to include all regulators and co-regulators
+  # Combine both vectors into one for all regulators and co-regulators
   reg_coreg_items <- c(coregs_items, regs_items) 
   
-  # Create a logical column 'TF' indicating whether the gene is either a known regulator or co-regulator
+  # Add a logical column 'TF' to indicate if the gene is a regulator or co-regulator
   getids_data$TF <- getids_data$gene_id %in% reg_coreg_items
   
-  # Return the results
+  # Return the updated dataframe
   return(getids_data)
 }
